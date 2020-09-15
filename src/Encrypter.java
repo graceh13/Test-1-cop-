018d2c6b67d2322107662f91e6e44788c8cdd263
Grace Hutchison <graceh5130@gmail.com>
2020-09-14 23:43:12 -0400

    Final Versions

---
+++ src/Encrypter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Class1 {
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a 4 digit number:");
+        String nums1 = scan.nextLine();
+
+    }
+
+}
