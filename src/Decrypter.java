3da860fdea0f64f8452099e9883611270c5ae76b
Grace Hutchison <graceh5130@gmail.com>
2020-09-14 23:58:08 -0400

    final2 versions

--- src/Decrypter.java
+++ src/Decrypter.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class Decrypter {
+import java.util.Scanner;
+import java.lang.String;
+
+public class Decrypter {
+    public static int [] getInput()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a 4 digit number to Decrypt:");
+        String nums = sc.nextLine(); //
+
+        int[] arr = new int[nums.length()]; // creates new array based off user input
+        for (int i = 0; i < nums.length(); i++)
+        {
+            arr[i] = nums.charAt(i) - '0'; // turns user input int into array
+        }
+    int x = 0;
+        sc.close();
+        return arr;
+    }
+
+    public static void unMod(int [] array) // undoes the mod on all elements in array
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            array[i] = array[i] + 10;
+        }
+
+
+    }
+
+    public static void sub7 (int [] array) // subtract 7 from all elements in array
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            array[i] = array[i] - 7;
+        }
+        for(int i = 0; i < 4; i++)
+        {
+            if(array[i] > 9)
+            {
+                array[i] = array[i] % 10;
+            }
+        }
+    }
+
+    public static void swap(int [] array) // swaps elements in array
+    {
+        int a = array[0];
+        int b = array [1];
+
+        array[0] = array[2];
+        array[1] = array[3];
+
+        array[2] = a;
+        array[3] = b;
+
+    }
+
+
+    public static void main(String args[])
+    {
+        int [] myArr = getInput();
+
+        swap(myArr);
+        unMod(myArr);
+        sub7(myArr);
+
+        for(int i = 0; i < 4; i++)
+        {
+            System.out.print(myArr[i]);
+        }
+    }
 }
--- src/Encrypter.java
+++ src/Encrypter.java
@@ -1,13 +1,62 @@
+import java.lang.String;
 import java.util.Scanner;
 
-public class Class1 {
-    public static void main(String args[])
-    {
-        Scanner scan = new Scanner(System.in);
+public class Encrypter {
 
-        System.out.println("Enter a 4 digit number:");
-        String nums1 = scan.nextLine();
+    public static int [] getInput()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a 4 digit number to Encrypt:");
+        String nums = sc.nextLine(); //
+
+        int[] arr = new int[nums.length()]; // creates new array based off user input
+        for (int i = 0; i < nums.length(); i++)
+        {
+            arr[i] = nums.charAt(i) - '0'; // turns user input int into array
+        }
+        int x = 0;
+        sc.close(); //closes scanner
+        return arr;
+    }
+
+    public static void addMod(int [] array) // adds 7 and mods all elements in array
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            array[i] = array[i] + 7; // adds 7 to all elements in array
+        }
+
+        for(int i = 0; i < 4; i++)
+        {
+            array[i] = array[i] % 10; // mod all elements in array
+        }
+    }
+
+    public static void swap(int [] array) // swaps elements positions in array
+    {
+        int a = array[0]; // stores first 2 elements in array in ints
+        int b = array [1];
+
+        array[0] = array[2]; // swaps places in array
+        array[1] = array[3];
+
+        array[2] = a; // takes stored elements and places them back in array
+        array[3] = b;
 
     }
 
+
+    public static void main(String args[])
+    {
+        int [] myArr = getInput();
+
+        addMod(myArr);
+        swap(myArr);
+
+        for(int i = 0; i < 4; i++)
+        {
+            System.out.print(myArr[i]);
+        }
+    }
 }
+
